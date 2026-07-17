0a36ee3ae6b49d8d329ca43929fe2feb0666f673
no7here <102915075+no7here@users.noreply.github.com>
2026-07-17 19:21:18 +0100

    Implement IP ban handling in WhitelistManager
    Added IP ban management functionality including kick method and IP list loading.

--- src/main/java/me/fallenbreath/velocitywhitelist/WhitelistManager.java
+++ src/main/java/me/fallenbreath/velocitywhitelist/WhitelistManager.java
@@ -8,18 +8,22 @@ import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.util.GameProfile;
 import me.fallenbreath.velocitywhitelist.config.Configuration;
+import me.fallenbreath.velocitywhitelist.config.IpList;
 import me.fallenbreath.velocitywhitelist.config.PlayerList;
 import me.fallenbreath.velocitywhitelist.utils.MojangAPI;
 import me.fallenbreath.velocitywhitelist.utils.UuidUtils;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -31,6 +35,7 @@ public class WhitelistManager
 	private final ProxyServer server;
 	private final PlayerList whitelist;
 	private final PlayerList blacklist;
+	private final IpList ipBanList;
 
 	public WhitelistManager(Logger logger, Configuration config, Path dataDirectory, ProxyServer server)
 	{
@@ -38,7 +43,18 @@ public class WhitelistManager
 		this.config = config;
 		this.whitelist = new PlayerList("Whitelist", dataDirectory.resolve("whitelist.yml"), this.config::isWhitelistEnabled);
 		this.blacklist = new PlayerList("Blacklist", dataDirectory.resolve("blacklist.yml"), this.config::isBlacklistEnabled);
+		this.ipBanList = new IpList("IpBanList", dataDirectory.resolve("ipbans.yml"), this.config::isIpBanEnabled);
 		this.server = server;
+	}
+
+	public Configuration getConfig()
+	{
+		return this.config;
+	}
+
+	public ProxyServer getServer()
+	{
+		return this.server;
 	}
 
 	public PlayerList getWhitelist()
@@ -51,10 +67,16 @@ public class WhitelistManager
 		return this.blacklist;
 	}
 
+	public IpList getIpBanList()
+	{
+		return this.ipBanList;
+	}
+
 	public void loadLists()
 	{
 		this.loadOneList(this.whitelist);
 		this.loadOneList(this.blacklist);
+		this.loadIpList(this.ipBanList);
 	}
 
 	private boolean isPlayerInList(GameProfile profile, PlayerList list)
@@ -164,7 +186,7 @@ public class WhitelistManager
 			profile = uuid.flatMap(this.server::getPlayer).map(Player::getGameProfile);
 		}
 
-		// uuid: get from value directly, or mojang api (lookup by input value)
+		// uuid: get from value directly, or mojang api (lookuped by input value)
 		// profile: get from server online player, lookuped by input value (name / uuid)
 
 		return switch (this.config.getIdentifyMode())
@@ -286,11 +308,91 @@ public class WhitelistManager
 
 	}
 
+	public void kickPlayersOnIp(String ipStr)
+	{
+		InetAddress targetAddr = null;
+		try
+		{
+			targetAddr = InetAddress.getByName(ipStr.trim());
+		}
+		catch (UnknownHostException ignored) {}
+
+		Component message = MiniMessage.miniMessage().deserialize(this.config.getIpBanKickMessage());
+
+		for (Player player : this.server.getAllPlayers())
+		{
+			InetSocketAddress address = player.getRemoteAddress();
+			if (address != null)
+			{
+				InetAddress playerAddr = address.getAddress();
+				boolean match = false;
+				if (targetAddr != null)
+				{
+					match = Arrays.equals(playerAddr.getAddress(), targetAddr.getAddress());
+				}
+				else
+				{
+					match = playerAddr.getHostAddress().equalsIgnoreCase(ipStr.trim());
+				}
+
+				if (match)
+				{
+					this.logger.info("Kicking connected player {} ({}) due to IP ban on {}", player.getUsername(), player.getUniqueId(), ipStr);
+					player.disconnect(message);
+				}
+			}
+		}
+	}
+
 	public void onPlayerLogin(LoginEvent event)
 	{
-		GameProfile profile = event.getPlayer().getGameProfile();
+		Player player = event.getPlayer();
+		GameProfile profile = player.getGameProfile();
+		InetSocketAddress remoteAddress = player.getRemoteAddress();
 
-		// Evaluate Blacklist FIRST
+		// 1. Evaluate IP Ban list
+		if (this.ipBanList.isActivated() && remoteAddress != null)
+		{
+			InetAddress ipAddress = remoteAddress.getAddress();
+			String ipString = ipAddress.getHostAddress();
+			if (this.ipBanList.checkIp(ipString))
+			{
+				Component message = MiniMessage.miniMessage().deserialize(this.config.getIpBanKickMessage());
+				event.setResult(ResultedEvent.ComponentResult.denied(message));
+				this.logger.info("Kicking player {} ({}) since their IP ({}) is banned", profile.getName(), profile.getId(), ipString);
+
+				// Blacklist on join toggle
+				if (this.config.isBlacklistOnIpBanJoin())
+				{
+					// Add player to blacklist
+					if (this.config.getIdentifyMode() == IdentifyMode.NAME)
+					{
+						if (this.blacklist.addPlayerName(profile.getName()))
+						{
+							this.logger.info("Automatically added player name {} to the blacklist due to joining on banned IP", profile.getName());
+							this.saveList(this.blacklist);
+						}
+					}
+					else
+					{
+						this.blacklist.computePlayerUUID(profile.getId(), (exists, oldName) -> {
+							var res = new PlayerList.PlayerUUIDComputeResult<Void>();
+							if (!exists || profile.getName() != null && (oldName == null || !oldName.equals(profile.getName())))
+							{
+								res.addNewValue = true;
+								res.newValue = profile.getName();
+								this.logger.info("Automatically added player UUID {} ({}) to the blacklist due to joining on banned IP", profile.getId(), profile.getName());
+							}
+							return res;
+						});
+						this.saveList(this.blacklist);
+					}
+				}
+				return; // Exit early if IP banned
+			}
+		}
+
+		// 2. Evaluate Blacklist SECOND
 		if (this.blacklist.isActivated() && this.isPlayerInBlacklist(profile))
 		{
 			Component message = MiniMessage.miniMessage().deserialize(this.config.getBlacklistKickMessage());
@@ -300,7 +402,7 @@ public class WhitelistManager
 			return; // Exit early if blacklisted
 		}
 
-		// Evaluate Whitelist after
+		// 3. Evaluate Whitelist THIRD
 		if (this.whitelist.isActivated() && !this.isPlayerInWhitelist(profile))
 		{
 			Component message = MiniMessage.miniMessage().deserialize(this.config.getWhitelistKickMessage());
@@ -333,7 +435,43 @@ public class WhitelistManager
 		}
 	}
 
+	public boolean loadIpList(IpList destList)
+	{
+		IpList newList = destList.createNewEmptyList();
+		try
+		{
+			if (!newList.getFilePath().toFile().isFile())
+			{
+				this.logger.info("Creating default empty {} file", newList.getName());
+				newList.save();
+			}
+			newList.load(this.logger);
+
+			destList.resetTo(newList);
+			return true;
+		}
+		catch (IOException e)
+		{
+			String msg = String.format("Failed to load the %s, the plugin might not work correctly!", newList.getName());
+			this.logger.error(msg, e);
+			return false;
+		}
+	}
+
 	public void saveList(PlayerList list)
+	{
+		try
+		{
+			list.save();
+		}
+		catch (IOException e)
+		{
+			String msg = String.format("Failed to save the %s", list.getName());
+			this.logger.error(msg, e);
+		}
+	}
+
+	public void saveIpList(IpList list)
 	{
 		try
 		{
