483f0bc494a80411e4c78b29394044fae1b7b157
no7here <102915075+no7here@users.noreply.github.com>
2026-07-23 14:55:24 +0100

    Bump plugin version + Bumped plugin version to v0.4.1

--- src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
+++ src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
@@ -4,6 +4,6 @@ public class PluginMeta
 {
 	public static final String ID = "velocitywhitelist";
 	public static final String NAME = "VelocityWhitelistExtended";
-	public static final String VERSION = "0.4.0";
+	public static final String VERSION = "0.4.1";
 	public static final String REPOSITORY_URL = "https://github.com/no7here/VelocityWhitelistExtended";
 }
